a2ff5fe772a6ae59fb7ebbb43494e5343c396e71
Njoga <wooskinner@yahoo.com>
2025-07-18 19:10:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add smooth scroll behavior
+document.querySelector('.cta-button').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('#products').scrollIntoView({ behavior: 'smooth' });
+});
